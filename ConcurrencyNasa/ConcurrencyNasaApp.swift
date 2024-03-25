317db515f4e881314f0c1e6290006da92ed80281
Axel Hernández <hplaxel@gmail.com>
2024-03-25 15:22:38 +0100

    Initial Commit

---
+++ ConcurrencyNasa/ConcurrencyNasaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConcurrencyNasaApp.swift
+//  ConcurrencyNasa
+//
+//  Created by Axel Amós Hernández Cárdenas on 25/03/24.
+//
+
+import SwiftUI
+
+@main
+struct ConcurrencyNasaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
